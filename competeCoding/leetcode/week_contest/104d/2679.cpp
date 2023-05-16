d584b164985750f7de402e1cb35729188b372658
clundro <859287553@qq.com>
2023-05-16 16:28:17 +0800

    104d done
    Signed-off-by: clundro <859287553@qq.com>

---
+++ competeCoding/leetcode/week_contest/104d/2679.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x) cout << "[debug]" #x << "=" << x << endl;
+#define fast_io            \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0)
+#define rep(i, a, b, c) for (int i = (a); i <= (b); i += (c))
+#define per(i, a, b, c) for (int i = (a); i >= (b); i -= (c))
+// #define endl "\n"
+#define vec vector
+using ll = long long;
+#define INF 0x3f3f3f3f
+
+inline int gcd(int a, int b) { return b > 0 ? gcd(b, a % b) : a; }
+
+vec<string> split_str(string is) {
+  vec<string> v;
+  while (is.find(" ") != string::npos) {
+    int found = is.find(" ");
+    v.push_back(is.substr(0, found));
+    is = is.substr(found + 1);
+  }
+  v.push_back(is);
+  return v;
+}
+
+vec<int> split(string is) {
+  vec<int> v;
+  while (is.find(" ") != string::npos) {
+    int found = is.find(" ");
+    v.push_back(stoi(is.substr(0, found)));
+    is = is.substr(found + 1);
+  }
+  v.push_back(stoi(is));
+  return v;
+}
+
+class UF {
+ public:
+  vec<int> item;
+  int cnt;
+  UF(int n) : cnt(n) {
+    item = vec<int>(n + 1, 0);
+    for (int i = 0; i < n; i++) item[i] = i;
+  }
+
+  int find(int x) {
+    if (x != item[x]) {
+      return (item[x] = find(item[x]));
+    }
+    return x;
+  }
+
+  void union_connect(int x, int y) {
+    int xitem = find(x);
+    int yitem = find(y);
+    if (xitem != yitem) {
+      item[yitem] = xitem;
+      cnt--;
+    }
+  }
+};
+
+struct lnode {
+  int v;
+  int next;
+  lnode(int v, int next) : v(v), next(next) {}
+};
+
+// inputs.erase(remove(inputs.begin(), inputs.end(), '['), inputs.end());
+
+class Solution {
+ public:
+  int matrixSum(vector<vector<int>>& nums) {
+    int m = nums.size(), n = nums[0].size();
+    for (auto& v : nums) {
+      sort(v.begin(), v.end());
+    }
+    int ans = 0;
+    for (int j = 0; j < n; j++) {
+      int mx = 0;
+      for (int i = 0; i < m; i++) {
+        mx = max(mx, nums[i][j]);
+      }
+      ans += mx;
+    }
+    return ans;
+  }
+};
+
+// #define TXT
+int main() {
+  fast_io;
+#ifdef TXT
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+#endif  // TXT
+
+#ifdef TXT
+  fclose(stdin);
+  fclose(stdout);
+#endif  // TXT
+  return 0;
+}
